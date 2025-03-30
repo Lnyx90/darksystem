c9eafc3854b16c3b1e87bb7eca24adcd8381a481
michi <michelleoliviah16@gmail.com>
2025-03-30 18:07:49 +0700

    ini yang benar yes

--- game/game.js
+++ game/game.js
@@ -1,4 +1,4 @@
-ddocument.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   const bgMusic = document.getElementById("bgMusic");
 
   document.body.addEventListener("click", () => {
